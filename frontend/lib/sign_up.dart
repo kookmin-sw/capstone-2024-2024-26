831a5d6f050d5f9a30a9c443cc151ea3975a9e62
ahnsuhyun <saker123456@kookmin.ac.kr>
2024-04-01 15:37:35 +0900

    feat: sign in and up page

--- frontend/lib/sign_up.dart
+++ frontend/lib/sign_up.dart
@@ -25,40 +25,225 @@ class _SignUpState extends State<SignUp> {
 
   String errorMessage = '';
 
+  List<String> _clubs = [
+    'Wink',
+    'D-Alpha',
+    'KoBot',
+    'Poska',
+  ];
+  List<String> _faculties = [
+    '소프트웨어 융합 대학',
+    '창의 공과 대학',
+    '조형 대학',
+    '경상 대학',
+  ];
+
+  String? _selectedClub;
+  String? _selectedFaculty;
+
   @override
   Widget build(BuildContext context) {
-    if (isloading) {
-      return LoadingScreen();
-    } else {
-      return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('회원가입'),
-          backgroundColor: const Color(0xFF3694A8),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-
-              Checkbox(
-              value: isChecked,
-              onChanged: (value) {
-              setState(() {
-              isChecked = value!;
-              });
-              },
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.only(right: 70.0, top: 20.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              '회원가입',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              const Text(
-              '개인정보 이용 및 약관에 동의하시겠습니까?',
-              style: TextStyle(color: Color(0xFF7A7A7A)),
+            ),
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(30.0),
+          child: Padding(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Divider(
+              color: Color(0xFFDFDFDF),
+              thickness: 1.0,
+            ),
+          ),
+        ),
       ),
-    ],
-  ),
-),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(right: 220),
+                child: const Text(
+                  '이름',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('이름을 입력하세요', controller: nameController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 220),
+                child: const Text(
+                  '학번',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('학번을 입력하세요', controller: studentIdController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 195),
+                child: const Text(
+                  '단과대학',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildDropdownField('단과대학', _faculties, _selectedFaculty),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 220),
+                child: const Text(
+                  '학과',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('학과를 입력하세요', controller: departmentController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 180),
+                child: const Text(
+                  '소속 동아리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildDropdownField('소속 동아리', _clubs, _selectedClub),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 170),
+                child: const Text(
+                  '이메일(아이디)',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('example@kookmin.ac.kr',
+                  controller: emailController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 195),
+                child: const Text(
+                  '비밀번호',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('비밀번호를 입력하세요',
+                  isPassword: true, controller: passwordController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 175),
+                child: const Text(
+                  '비밀번호 확인',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('비밀번호를 입력하세요',
+                  isPassword: true, controller: confirmPasswordController),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 180),
+                child: const Text(
+                  '휴대폰 번호',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.13,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              buildInputField('휴대폰 번호를 입력하세요', controller: phoneController),
+              const SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.only(left: 35.0), // Row 위쪽에 10 픽셀의 여백 추가
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    const Text(
+                      '개인정보 이용 및 약관에 동의하시겠습니까?',
+                      style: TextStyle(color: Color(0xFF7A7A7A)),
+                    ),
+                  ],
+                ),
+              ),
               Container(
                   width: double.infinity,
                   child: Text(
@@ -89,7 +274,6 @@ class _SignUpState extends State<SignUp> {
                       fontSize: 12,
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w700,
-
                     ),
                   ),
                 ),
@@ -127,16 +311,72 @@ class _SignUpState extends State<SignUp> {
 
   Widget buildInputField(String labelText,
       {bool isPassword = false, TextEditingController? controller}) {
-    return TextField(
-      obscureText: isPassword,
-      controller: controller,
-      style: const TextStyle(fontSize: 13),
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: const TextStyle(color: Color(0xFF9C9C9C)),
-        focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFF9C9C9C)),
+    return Container(
+      width: 265.75,
+      height: 28.97,
+      decoration: ShapeDecoration(
+        color: Color(0x4FECECEC),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
+      ),
+      child: TextField(
+        obscureText: isPassword,
+        controller: controller,
+        style: const TextStyle(fontSize: 13),
+        decoration: InputDecoration(
+          hintText: labelText,
+          hintStyle: const TextStyle(color: Color(0xFF9C9C9C)),
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF9C9C9C)),
+          ),
+          border: InputBorder.none,
+          filled: true,
+          fillColor: Color.fromARGB(255, 246, 246, 246),
         ),
+      ),
+    );
+  }
+
+  Widget buildDropdownField(
+      String labelText, List<String> items, String? value) {
+    return Container(
+      width: 265.75,
+      height: 28.97,
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          hintText: labelText,
+          hintStyle: TextStyle(
+            color: Color(0xFF9C9C9C),
+            fontSize: 13,
+          ),
+          filled: true,
+          fillColor: Color.fromARGB(255, 246, 246, 246),
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(2.0),
+            borderSide: BorderSide(color: Colors.transparent),
+          ),
+        ),
+        value: value,
+        onChanged: (newValue) {
+          setState(() {
+            if (labelText == '소속 동아리') {
+              _selectedClub = newValue;
+            } else if (labelText == '단과대학') {
+              _selectedFaculty = newValue;
+            }
+          });
+        },
+        items: items.map<DropdownMenuItem<String>>((String item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            child: Text(
+              item,
+              style: TextStyle(fontSize: 12),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
