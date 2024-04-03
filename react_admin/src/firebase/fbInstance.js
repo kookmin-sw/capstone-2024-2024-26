9f83f7610d5959ee956cab63b7d189c6f1ac08d8
JohnJung-1017 <jungih1017@gmail.com>
2024-04-03 15:22:06 +0900

    style: login.jsx
    로그인 페이지 css작업

--- react_admin/src/firebase/fbInstance.js
+++ react_admin/src/firebase/fbInstance.js
@@ -9,10 +9,8 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-    // measurementId: REACT_APP_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 export const authService = getAuth();
--- react_admin/src/pages/login.jsx
+++ react_admin/src/pages/login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { authService } from '../firebase/fbInstance';
 import { useNavigate } from "react-router-dom";
+import kookmin_logo from '../image/kookmin_logo.jpg';
 import style from "../styles/login.css";
 
 const Login = () => {
@@ -26,29 +27,53 @@ const Login = () => {
         }
     }
 
+    const backgroundImageStyle = {
+        backgroundImage: `url(${kookmin_logo})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      };
+
     return (
-        <div>
+      <div className='login_background'>
+       <div className='login_container'>
+            {/* <h2>K-SharePlace 관리자 웹</h2> */}
+        <div className='login_box'>
+            <h2>K-SharePlace 관리자 웹</h2>
             <form onSubmit={onSubmit}>
-                <input
-                    name="email"
-                    type="text"
-                    placeholder='Email'
-                    required
-                    value={email}
-                    onChange={onChange} />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    value={password}
-                    onChange={onChange} />
-                <input
-                    type="submit"
-                    value="Login"/>
+                <div className='form_group'>
+                    <label htmlFor="Email">관리자 아이디</label>
+                        <input
+                            className='login_box_input'
+                            name="email"
+                            type="text"
+                            placeholder='exmaple@kookmin.ac.kr'
+                            required
+                            value={email}
+                            onChange={onChange} />
+                </div>
+                <div className='form_group'>
+                        <label htmlFor="Password">비밀번호</label>
+                        <input
+                            className='login_box_input'
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            required
+                            value={password}
+                            onChange={onChange} />
+                </div>
+                        <input
+                            className='login_box_button'
+                            type="submit"
+                            value="Login"/>
             </form>
-            {error && <p>{error}</p>}
+            {error && <p className='login_error'>{ "아이디 혹은 비밀번호가 잘못되었습니다." }</p>}
         </div>
+        <div className='login_image' style={backgroundImageStyle}></div>
+       </div>
+      </div>
+
     )
 }
 
