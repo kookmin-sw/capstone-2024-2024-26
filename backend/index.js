a0f6a5366b3ae429e838a9e90b750bf33892182e
Uiseokjung <luki2010@naver.com>
2024-04-26 16:35:09 +0900

    #32, #33, #34 fixed: Configure administrator mode path

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,29 @@ import bodyParser from "body-parser";
 import router from "./routes/authRoutes.js";
 import reserveClub from "./routes/reserveclub.js";
 import reserveroom from "./routes/reserveroom.js";
+import adminAuth from "./admin/routes/adminAuth.js";
+import adminClub from "./admin/routes/adminClub.js";
+import adminRoom from "./admin/routes/adminRoom.js";
 
 const port = 3000;
 
-const app2 = express();
+const app = express();
 
-app2.use(express.json());
-app2.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.json());
 // 회원가입, 로그인, 로그아웃
-app2.use("/auth", router);
+app.use("/auth", router);
 // 동아리 예약 관련 api
-app2.use("/reserveclub", reserveClub);
+app.use("/reserveclub", reserveClub);
 // 강의실 예약 관련 api
-app2.use("/reserveroom", reserveroom);
+app.use("/reserveroom", reserveroom);
+// 관리자 회원 관리 api
+app.use("/adminAuth", adminAuth);
+// 관리자 동아리방 관리 api
+app.use("/adminClub", adminClub);
+// 관리자 강의실 관리 api
+app.use("/adminRoom", adminRoom);
 // 서버 시작
-app2.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
