2e2e631c131be02d4ca77549caa9ec881e6544a4
Uiseokjung <luki2010@naver.com>
2024-04-28 23:27:32 +0900

    #34 fixed: fixed collection route for reserving classroom

--- backend/routes/reserveroom.js
+++ backend/routes/reserveroom.js
@@ -45,108 +45,128 @@ reserveroom.post("/", async (req, res) => {
   try {
     // 사용자 정보 가져오기
     const userDoc = await getDoc(doc(db, "users", userId));
+
     if (!userDoc.exists()) {
       return res.status(404).json({ error: "User not found" });
     }
     const userData = userDoc.data();
 
-    // 예약된 시간대와 강의실 정보 확인
-    const existingroomReservationSnapshot = await getDocs(
-      collection(db, "reservationRoom"),
-      where("date", "==", date),
+    const collectionName = `${userData.faculty}_${userData.department}_Classroom`;
+
+    // 문서 ID에 roomId와 같은 문자열이 포함되어 있는지 확인
+    const existingReservationSnapshot = await getDocs(
+      collection(db, `${collectionName}`),
       where("roomId", "==", roomId)
     );
 
-    // 겹치는 예약이 있는지 확인
-    const overlappingReservation = existingroomReservationSnapshot.docs.find(
-      (doc) => {
-        const reservation = doc.data();
-
-        // 기존 예약의 시작 시간과 끝 시간
-        const existingStartTime = reservation.startTime;
-        const existingEndTime = reservation.endTime;
-        const existingDate = reservation.date;
-        const existingRoomId = reservation.roomId;
-        const startTimeRoom = startTime;
-        const endTimeRoom = endTime;
-
-        // 예약 시간이 같은 경우 또는 기존 예약과 겹치는 경우 확인
-        if (
-          (existingDate == date &&
-            startTimeRoom == existingStartTime &&
-            endTimeRoom == existingEndTime &&
-            roomId == existingRoomId) ||
-          (existingDate == date &&
-            roomId == existingRoomId &&
-            startTimeRoom < existingEndTime &&
-            endTimeRoom > existingStartTime)
-        ) {
-          return true;
-        }
-        return false;
-      }
+    const existingReservation = existingReservationSnapshot.docs.find((doc) =>
+      doc.id.includes(roomId)
     );
 
-    // 겹치는 예약이 있는 경우 에러 반환
-    if (overlappingReservation) {
-      return res
-        .status(401)
-        .json({ error: "The room is already reserved for this time" });
-    }
-
-    // 학생들의 학번을 공백을 기준으로 분할하여 리스트를 만듦
-    const studentIdList = studentIds.split(" ");
-    if (studentIdList.length != numberOfPeople) {
-      return res.status(400).json({
-        error:
-          "The numberOfPeople does not match the number of given studentIds",
-      });
-    }
-
-    // 각 학생의 정보를 가져오는 비동기 함수
-    const getUserInfoPromises = studentIdList.map(async (studentId) => {
-      const userQuerySnapshot = await getDocs(
-        query(collection(db, "users"), where("studentId", "==", studentId))
+    // roomId와 같은 문자열이 포함되어 있는 경우 예약 진행
+    if (existingReservation) {
+      // 예약된 시간대와 좌석 확인
+      const existingReservationsSnapshot = await getDocs(
+        collection(db, `${collectionName}`),
+        where("date", "==", date),
+        where("roomId", "==", roomId)
       );
-      if (!userQuerySnapshot.empty) {
-        const userData = userQuerySnapshot.docs[0].data();
-        return {
-          studentId: studentId,
-          name: userData.name,
-          faculty: userData.faculty,
-        };
-      } else {
-        throw new Error(`User with ID ${studentId} not found`);
+
+      // 겹치는 예약이 있는지 확인
+      const overlappingReservation = existingReservationsSnapshot.docs.find(
+        (doc) => {
+          const reservation = doc.data();
+          // 기존 예약의 시작 시간과 끝 시간
+          const existingStartTime = reservation.startTime;
+          const existingEndTime = reservation.endTime;
+          const existingDate = reservation.date;
+          const existingRoomId = reservation.roomId;
+          const startTimeClub = startTime;
+          const endTimeClub = endTime;
+
+          // 예약 시간이 같은 경우 또는 기존 예약과 겹치는 경우 확인
+          if (
+            (existingDate == date &&
+              startTimeClub == existingStartTime &&
+              endTimeClub == existingEndTime &&
+              roomId == existingRoomId) ||
+            (existingDate == date &&
+              roomId == existingRoomId &&
+              startTimeClub < existingEndTime &&
+              endTimeClub > existingStartTime)
+          ) {
+            return true;
+          }
+          return false;
+        }
+      );
+
+      // 겹치는 예약이 있는 경우 에러 반환
+      if (overlappingReservation) {
+        return res
+          .status(401)
+          .json({ error: "The room is already reserved for this time" });
       }
-    });
 
-    // 비동기 함수들을 병렬로 실행하여 학생 정보를 가져옵니다.
-    const studentInfoList = await Promise.all(getUserInfoPromises);
+      // 학생들의 학번을 공백을 기준으로 분할하여 리스트를 만듦
+      const studentIdList = studentIds.split(" ");
+      if (studentIdList.length != numberOfPeople) {
+        return res.status(400).json({
+          error:
+            "The numberOfPeople does not match the number of given studentIds",
+        });
+      }
 
-    // 전에 사용자가 한 예약이 있는지 확인
-    const existingMyReservationSnapshot = await getDocs(
-      collection(db, "reservationRoom"),
-      where("userEmail", "==", userData.email)
-    );
+      // 각 학생의 정보를 가져오는 비동기 함수
+      const getUserInfoPromises = studentIdList.map(async (studentId) => {
+        const userQuerySnapshot = await getDocs(
+          query(collection(db, "users"), where("studentId", "==", studentId))
+        );
+        if (!userQuerySnapshot.empty) {
+          const userData = userQuerySnapshot.docs[0].data();
+          return {
+            studentId: studentId,
+            name: userData.name,
+            faculty: userData.faculty,
+          };
+        } else {
+          throw new Error(`User with ID ${studentId} not found`);
+        }
+      });
 
-    // 문서 컬렉션에 uid로 구분해주기(덮어쓰이지않게 문서 개수에 따라 번호 부여)
-    const reservationCount = existingMyReservationSnapshot.size;
-    // 겹치는 예약이 없으면 예약 추가
-    await setDoc(doc(db, "reservationRoom", `${userId}_${reservationCount}`), {
-      mainName: userData.name,
-      roomId: roomId,
-      date: date,
-      startTime: startTime,
-      endTime: endTime,
-      usingPurpose: usingPurpose,
-      numberOfPeople: numberOfPeople,
-      studentIds: studentIdList,
-      studentNames: studentInfoList.map((student) => student.name),
-      studentFaculty: studentInfoList.map((student) => student.faculty),
-    });
+      // 비동기 함수들을 병렬로 실행하여 학생 정보를 가져옵니다.
+      const studentInfoList = await Promise.all(getUserInfoPromises);
 
-    // 예약 성공 시 응답
-    res.status(201).json({ message: "Reservation room created successfully" });
+      // 전에 사용자가 한 예약이 있는지 확인
+      const existingMyReservationSnapshot = await getDocs(
+        collection(db, `${collectionName}`),
+        where("userEmail", "==", userData.email)
+      );
+
+      // 문서 컬렉션에 uid로 구분해주기(덮어쓰이지않게 문서 개수에 따라 번호 부여)
+      const reservationCount = existingMyReservationSnapshot.size;
+      // 겹치는 예약이 없으면 예약 추가
+      await setDoc(
+        doc(db, `${collectionName}`, `${roomId}_${userData.studentId}_${reservationCount}`),
+        {
+          mainName: userData.name,
+          roomId: roomId,
+          date: date,
+          startTime: startTime,
+          endTime: endTime,
+          usingPurpose: usingPurpose,
+          numberOfPeople: numberOfPeople,
+          studentIds: studentIdList,
+          studentNames: studentInfoList.map((student) => student.name),
+          studentFaculty: studentInfoList.map((student) => student.faculty),
+        }
+      );
+
+      // 예약 성공 시 응답
+      res
+        .status(201)
+        .json({ message: "Reservation room created successfully" });
+    }
   } catch (error) {
     // 오류 발생 시 오류 응답
     console.error("Error creating reservation room", error);
