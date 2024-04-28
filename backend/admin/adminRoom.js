5830095ff07bf00241a75458bdef1a81de729dff
Uiseokjung <luki2010@naver.com>
2024-04-28 23:07:11 +0900

    #34 feat: administrator mode create classroom

--- backend/admin/adminRoom.js
+++ backend/admin/adminRoom.js
@@ -1,32 +1,47 @@
 import {
-    collection,
-    getFirestore,
-    getDoc,
-    setDoc,
-    doc,
-    getDocs,
-    where,
-    query,
-    deleteDoc,
-    updateDoc,
-  } from "firebase/firestore";
-  import { initializeApp } from "firebase/app";
-  import express from "express";
-  import dotenv from "dotenv";
-  dotenv.config();
-  const firebaseConfig = {
-    apiKey: process.env.FLUTTER_APP_apikey,
-    authDomain: process.env.FLUTTER_APP_authDomain,
-    projectId: process.env.FLUTTER_APP_projectId,
-    storageBucket: process.env.FLUTTER_APP_storageBucket,
-    messagingSenderId: process.env.FLUTTER_APP_messagingSenderId,
-    appId: process.env.FLUTTER_APP_appId,
-    measurementId: process.env.FLUTTER_APP_measurementId,
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  
-  const adminRoom = express.Router();
+  collection,
+  getFirestore,
+  getDoc,
+  setDoc,
+  doc,
+  getDocs,
+  where,
+  query,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const firebaseConfig = {
+  apiKey: process.env.FLUTTER_APP_apikey,
+  authDomain: process.env.FLUTTER_APP_authDomain,
+  projectId: process.env.FLUTTER_APP_projectId,
+  storageBucket: process.env.FLUTTER_APP_storageBucket,
+  messagingSenderId: process.env.FLUTTER_APP_messagingSenderId,
+  appId: process.env.FLUTTER_APP_appId,
+  measurementId: process.env.FLUTTER_APP_measurementId,
+};
 
-  export default adminRoom;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const adminRoom = express.Router();
+
+// 관리자 강의실 생성
+adminRoom.post("/create/room", isAdmin, async (req, res) => {
+  const { faculty, department, roomId } = req.body;
+  try {
+    await setDoc(doc(db, `${faculty}_${department}_Club`, `${roomId}`), {
+      adminMessage: `Admin has set up ${roomId} classroom.`,
+    });
+
+    res.status(200).json({ message: "Register Classroom successfully" });
+  } catch (error) {
+    console.error("Error registering Classroom:", error);
+    res.status(500).json({ error: "Failed to register Classroom" });
+  }
+});
+
+export default adminRoom;
