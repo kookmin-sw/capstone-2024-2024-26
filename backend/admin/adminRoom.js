504b404f6894f8cdd51b9593f42792e413aa1a72
Uiseokjung <luki2010@naver.com>
2024-04-28 23:12:54 +0900

    #34 feat: checking admin email

--- backend/admin/adminRoom.js
+++ backend/admin/adminRoom.js
@@ -29,6 +29,22 @@ const db = getFirestore(app);
 
 const adminRoom = express.Router();
 
+function isAdmin(req, res, next) {
+  const { email } = req.body;
+  // 관리자 이메일
+  const adminEmail = "admin@kookmin.ac.kr";
+
+  // 이메일이 관리자 이메일과 일치하는지 확인
+  if (email === adminEmail) {
+    // 관리자인 경우 다음 미들웨어로 진행
+    console.log("isAdmin OK");
+    next();
+  } else {
+    // 관리자가 아닌 경우 권한 없음 응답
+    res.status(403).json({ error: "Unauthorized: You are not an admin " });
+  }
+}
+
 // 관리자 강의실 생성
 adminRoom.post("/create/room", isAdmin, async (req, res) => {
   const { faculty, department, roomId } = req.body;
