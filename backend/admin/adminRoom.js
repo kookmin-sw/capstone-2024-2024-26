b1841e666834fb940b553db3a80b0823f304df43
Uiseokjung <luki2010@naver.com>
2024-04-29 14:02:43 +0900

    #33 feat: Agree reservation room request

--- backend/admin/adminRoom.js
+++ backend/admin/adminRoom.js
@@ -60,4 +60,47 @@ adminRoom.post("/create/room", isAdmin, async (req, res) => {
   }
 });
 
+adminRoom.post("/agree", isAdmin, async (req, res) => {
+  const { userId, boolAgree, reserveroomUID } = req.body;
+  try {
+    if (boolAgree === true) {
+      // 사용자 정보 가져오기
+      const userDoc = await getDoc(doc(db, "users", userId));
+
+      if (!userDoc.exists()) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      const userData = userDoc.data();
+
+      // 컬렉션 이름 설정
+      const collectionNameQueue = `${userData.faculty}_Classroom_queue`;
+
+      // 예약 강의실 정보 가져오기
+      const reserveroomDoc = await getDoc(doc(db, collectionNameQueue, reserveroomUID));
+
+      if (!reserveroomDoc.exists()) {
+        return res.status(404).json({ error: "Reservation not found" });
+      }
+
+      // 강의실 예약 정보를 등록할 컬렉션 이름 설정
+      const classroomCollectionName = `${userData.faculty}_Classroom`;
+
+      // 강의실 예약 정보 등록
+      await setDoc(doc(db, classroomCollectionName, reserveroomUID), reserveroomDoc.data());
+
+      // boolAgree 값을 true로 변경하여 저장
+      await updateDoc(doc(db, classroomCollectionName, reserveroomUID), {
+        boolAgree: true
+      });
+
+      res.status(200).json({ message: "Register Classroom successfully" });
+    } else {
+      res.status(400).json({ error: "Administrator denied your reservation" });
+    }
+  } catch (error) {
+    console.error("Error registering Classroom:", error);
+    res.status(500).json({ error: "Failed to register Classroom" });
+  }
+});
+
 export default adminRoom;
