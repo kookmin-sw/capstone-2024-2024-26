4a9eb912d359af1d5fb34af0fd56f07e86d5d850
JohnJung-1017 <jungih1017@gmail.com>
2024-05-04 18:10:04 +0900

    design: main-content <div> 수정
    sideBar 글씨 크기
    main-content div
    공간관리 UI 구성

--- react_admin/src/pages/pageManagement.jsx
+++ react_admin/src/pages/pageManagement.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import Sidebar from './sideBar';
 import Banner from './banner';
+import '../styles/pageManagement.css';
 
 const PageManagement = () => {
     return (
@@ -8,8 +10,29 @@ const PageManagement = () => {
           <Banner /> {/* 배너 컴포넌트를 최상단에 표시 */}
           <div className="sidebar-and-content"> {/* 사이드바와 내용을 담는 컨테이너 */}
             <Sidebar /> {/* 사이드바를 좌측에 표시 */}
-            <div className="main-content"> {/* 메인 작업물을 표시하는 컨테이너 */}
-            </div>
+            <div className="main-content">
+              <div className='addition_container'>
+                <div className='addition_box'>
+                  <div className='addition_banner'>
+                    <p className='addition_title'>강의실 관리</p>
+                    <button className='addition_room_button'>강의실 추가</button>
+                  </div>
+                  <hr></hr>
+                  <div className='addition_chart'>
+                  </div>
+                </div>
+                <div className='blank'></div>
+                <div className='addition_box'>
+                  <div className='addition_banner'>
+                    <p className='addition_title'>공유공간 관리</p>
+                    <button className='addition_club_button'>공유공간 추가</button>
+                  </div>
+                  <hr></hr>
+                  <div className='addition_chart'>
+                  </div>
+                </div>
+              </div>
+          </div>
           </div>
         </div>
       );
