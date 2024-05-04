f05a48954979004198ab0d15348fb576381ca970
jaehoon222 <jhoon5061@naver.com>
2024-05-04 15:04:52 +0900

    new

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def home():
+#     return "ㄱㄱ"
+
+
+# @app.route('/test')
+# def test():
+#     return "테스트 동작완료"
+
 @app.route('/')
 def home():
-    return "ㄱㄱ"
+    return render_template("index.html")
 
 
-@app.route('/test')
+@app.route('/test', methods=["GET", "POST"])
 def test():
-    return "테스트 동작완료"
+    if request.method == "POST":
+        try:
+            number = int(request.form['number'])
+            result = number + 1
+            return render_template("result.html", result=result)
+        except ValueError:
+            return "Invalid input. Please enter a number."
+    return render_template("test.html")
 
 
 if __name__ == '__main__':
