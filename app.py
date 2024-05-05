3563b9ef8dbec9ff567a5d0966ce84e059bb9ceb
jaehoon222 <jhoon5061@naver.com>
2024-05-05 19:37:41 +0900

    change

--- app.py
+++ app.py
@@ -1,32 +1,66 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
+import base64
+from PIL import Image
+import io
+from count import count
+from image_class import classification
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return "ㄱㄱ"
-
-
-# @app.route('/test')
-# def test():
-#     return "테스트 동작완료"
-
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return "서버실행 테스트"
+
+@app.route('/test')
+def index():
+    return "동작완료"
+
+# 공유공간 정보는 계속 받아와서 따로 계속 혼잡도 리스트로 만들어뒀다가 
+# 요청들어오면 보내주기 
+@app.route('/api/count', methods=['POST'])
+def handle_request():
+    data = request.get_json()
+    if not data or 'image' not in data or 'info' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+    
 
 
-@app.route('/test', methods=["GET", "POST"])
-def test():
-    if request.method == "POST":
-        try:
-            number = int(request.form['number'])
-            result = number + 1
-            return render_template("result.html", result=result)
-        except ValueError:
-            return "Invalid input. Please enter a number."
-    return render_template("test.html")
+    # 이미지 데이터와 추가 정보를 추출
+    base64_image = data['image']
+    info = data['info']
 
+    
+    # 이 코드는 진짜 인코딩 된 값 불러 올때 사용
+    image_data = base64.b64decode(base64_image)
+    image = Image.open(io.BytesIO(image_data)).convert('RGB')
+    result = count(image, info)
+
+
+    # 결과에 따라 firebase에 저장 or flutter에 재요청
+    # 웹에도 정보 보내야 될 수도 이;ㅆ음...
+    
+    # 이건 진짜 사용할때
+    # return jsonify(result)
+
+
+    # 이건 테스트용
+    return render_template('display.html', result=result)
+
+
+#이미지 청소상태 체크
+@app.route('/api/class', methods=['POST'])
+def classi():
+    result = classification()
+    
+    # 이것도 원래는 이미지 넣어줘야됨
+    #일단 나중에
+
+    # 여기서 데베 저장까지
+    #이 코드는 ㄹㅇ 연동할때
+    # return jsonify(result)
+
+    #테스트용
+    return render_template('display.html', result=result["score"])
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ image_class.py
@@ -0,0 +1,130 @@
+import torch
+import numpy as np
+from torchvision import models, transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.metrics.pairwise import cosine_similarity
+import timm
+
+
+
+# 이미지 로드 및 전처리 함수
+def load_image(image_path):
+    # 이미지 전처리
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    
+    input_image = Image.open(image_path).convert('RGB')
+    processed_image = preprocess(input_image)
+    processed_image = processed_image.unsqueeze(0)  # 차원 추가
+    
+    return processed_image
+
+#임베딩 추출함수
+def get_vit_embeddings(tensor_image, vit):
+    with torch.no_grad():
+        image_embedding = vit(tensor_image)
+    return image_embedding
+
+def get_resnet_embeddings(tensor_image, resnet):
+    with torch.no_grad():
+        image_embedding = resnet(tensor_image)
+    return image_embedding
+
+def get_inception_embeddings(tensor_image, inception):
+    with torch.no_grad():
+        image_embedding = inception(tensor_image)
+    return image_embedding
+
+
+# 두 이미지 간의 코사인 유사도 계산 함수
+def get_similarity_score(first_image_path, second_image_path, vit, resnet, inception):
+    first_image_tensor = load_image(first_image_path)
+    second_image_tensor = load_image(second_image_path)
+    
+
+    first_vit_embedding = get_vit_embeddings(first_image_tensor, vit)
+    second_vit_embedding = get_vit_embeddings(second_image_tensor, vit)
+    
+    first_resnet_embedding = get_resnet_embeddings(first_image_tensor, resnet)
+    second_resnet_embedding = get_resnet_embeddings(second_image_tensor, resnet)
+
+    first_inception_embedding = get_inception_embeddings(first_image_tensor, inception)
+    second_inception_embedding = get_inception_embeddings(second_image_tensor, inception)
+
+    first_vit_vector = first_vit_embedding.view(first_vit_embedding.size(0), -1)
+    second_vit_vector = second_vit_embedding.view(second_vit_embedding.size(0), -1)
+
+    first_resnet_vector = first_resnet_embedding.view(first_resnet_embedding.size(0), -1)
+    second_resnet_vector = second_resnet_embedding.view(second_resnet_embedding.size(0), -1)
+
+    first_inception_vector = first_inception_embedding.view(first_inception_embedding.size(0), -1)
+    second_inception_vector = second_inception_embedding.view(second_inception_embedding.size(0), -1)
+    
+    
+
+    vit_score = torch.nn.functional.cosine_similarity(first_vit_vector, second_vit_vector)
+    vit_score = round(vit_score.item(), 3)
+
+    resnet_score = torch.nn.functional.cosine_similarity(first_resnet_vector, second_resnet_vector)
+    resnet_score = round(resnet_score.item(), 3)
+
+    inception_score = torch.nn.functional.cosine_similarity(first_inception_vector, second_inception_vector)
+    inception_score = round(inception_score.item(), 3)
+
+    score = { "vit_score" : vit_score, "resnet_score" : resnet_score, "inception_score" : inception_score}
+
+    return score
+
+
+def classification():
+    resnet = models.resnet34()
+    resnet.load_state_dict(torch.load('./model/resnet34.pth'))
+    resnet.eval()
+
+    inception = models.googlenet()
+    inception.load_state_dict(torch.load('./model/googlenet.pth'))
+    inception.eval()
+
+
+    vit = timm.create_model('deit_tiny_patch16_224', pretrained=True)
+    vit.load_state_dict(torch.load('./model/deit_tiny.pth'))
+    vit.eval()
+
+
+    
+    #이미지 경로
+    #첫번째 이미지는 내 폴더에서, 두번째는 사진찍은거 받아와서
+    before_image = './model/image/6.jpg'
+    after_image = './model/image/7.jpg'
+
+
+    # 유사도 점수 계산 및 출력
+    score = get_similarity_score(before_image, after_image, vit, resnet, inception)
+
+    #각각의 임계값을 설정 후 셋중 두개 이상인걸로 ㄱㄱ
+    # 동아리방 임계값 0.75, 0.8, 0.7로 설정
+    #강의실 임계값도 사진 가져와서 테스트 후 정해야됨
+    count = 0
+    if score["vit_score"] > 0.75:
+        count+=1
+
+    if score["resnet_score"] > 0.8:
+        count+=1
+
+    if score["inception_score"] > 0.7:
+        count+=1
+
+    
+    #마지막 출력
+    if count >=2:
+        # print("합격")
+        return {"score" : "청소완료"}
+    else:
+        # print("청소해")
+        return {"score" : "청소다시하셈"}
