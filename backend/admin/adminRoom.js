728bf07c7a80dc787e851110dd4a32b87101ddd7
Uiseokjung <luki2010@naver.com>
2024-05-10 00:15:23 +0900

    #34 fixed: add location data in creating conference

--- backend/admin/adminRoom.js
+++ backend/admin/adminRoom.js
@@ -53,6 +53,7 @@ adminRoom.post("/create/room", isAdmin, async (req, res) => {
   const {
     faculty,
     roomName,
+    location,
     available_People,
     available_Time,
     conferenceImage,
@@ -72,6 +73,7 @@ adminRoom.post("/create/room", isAdmin, async (req, res) => {
     const data = {
       faculty: faculty,
       roomName: roomName,
+      location: location,
       available_People: available_People,
       available_Time: available_Time,
       conferenceImage: conferenceImage,
